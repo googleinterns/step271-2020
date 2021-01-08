bf0d9508e85e2b71c6246abdb23c7925d6ab9756
Therese Nieva <theresenieva@gmail.com>
2021-01-08 03:36:01 +0000

    Add basic map to the page.

---
+++ prototype/src/main/webapp/chooseLocation.js
@@ -0,0 +1,17 @@
+let map;
+
+/* Editable marker that displays when a user clicks in the map. */
+let editMarker;
+
+/** Creates a map that allows users to add markers. */
+function createMap() {
+  map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: -33.8688, lng: 151.2093}, zoom: 12});
+
+  map.addListener('click', (event) => {
+    createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+  }); 
+}
+
+
