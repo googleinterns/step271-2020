9ea1583c23b59828e5710e65740d98a99b1edbe7
Anna Giang <annagiang@google.com>
2021-01-08 17:24:34 +1100

    Refactor voteTime to remove repeated code
    Loop through all inputs in the vote form instead of repeating the actions for each input separately.`

--- prototype/src/main/webapp/script.js
+++ prototype/src/main/webapp/script.js
@@ -1,27 +1,16 @@
+PROPOSED_TIMES_NO = 3; // Initially we hardcoded 3 different times suggestions
+
 function voteTime() {
   // get the answers from the form and update votes on screen
   // NOTE this data would be sent to a servlet in MVP and the data fetched to update UI
-  let inputs = document.getElementById("vote-time").elements;
-  let time1 = inputs["time-1"];
-  if (time1.checked === true) {
-    document.getElementById("time-1-vote").innerText = (
-      parseInt(document.getElementById("time-1-vote").innerText) + 1
-    ).toString();
-    // disable radio button, since user may only vote once for each time
-    time1.disabled = true;
-  }
-  let time2 = inputs["time-2"];
-  if (time2.checked === true) {
-    document.getElementById("time-2-vote").innerText = (
-      parseInt(document.getElementById("time-2-vote").innerText) + 1
-    ).toString();
-    time2.disabled = true;
-  }
-  let time3 = inputs["time-3"];
-  if (time3.checked === true) {
-    document.getElementById("time-3-vote").innerText = (
-      parseInt(document.getElementById("time-3-vote").innerText) + 1
-    ).toString();
-    time3.disabled = true;
+  let inputs = document.getElementById('vote-time').elements;
+  for (let i = 0; i < PROPOSED_TIMES_NO; i++) {
+    if (inputs[i].checked === true) {
+      document.getElementById(inputs[i].id + '-vote').innerText = (
+          parseInt(document.getElementById(inputs[i].id + '-vote').innerText) + 1
+      ).toString();
+      // disable radio button, since user may only vote once for each time
+      inputs[i].disabled = true;
+    }
   }
 }
