86f23935f65a0bf96dece70080c3bdc834fff8b2
Therese Nieva <theresenieva@gmail.com>
2021-01-08 03:40:25 +0000

    Display textbox for input when map is clicked

--- prototype/src/main/webapp/chooseLocation.js
+++ prototype/src/main/webapp/chooseLocation.js
@@ -14,4 +14,50 @@ function createMap() {
   }); 
 }
 
+/** Creates a marker that shows a textbox the user can edit. */
+function createMarkerForEdit(lat, lng) {
+  // If we're already showing an editable marker, then remove it.
+  if (editMarker) {
+    editMarker.setMap(null);
+  }
+
+  editMarker =
+      new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+
+  const infoWindow =
+      new google.maps.InfoWindow({content: buildInfoWindowInput(lat, lng)});
+
+  // When the user closes the editable info window, remove the marker.
+  google.maps.event.addListener(infoWindow, 'closeclick', () => {
+    editMarker.setMap(null);
+  });
+
+  infoWindow.open(map, editMarker);
+}
+
+/**
+ * Builds and returns HTML elements that show an editable textbox and a submit
+ * button.
+ */
+function buildInfoWindowInput(lat, lng) {
+  const prompt = document.createElement('span');
+  prompt.innerText = "Enter location title:"
+
+  const textBox = document.createElement('textarea');
+  const button = document.createElement('button');
+  button.appendChild(document.createTextNode('CONFIRM'));
+
+  button.onclick = () => {
+    editMarker.setMap(null);
+  };
+
+  const containerDiv = document.createElement('div');
+  containerDiv.appendChild(prompt);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(textBox);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(button);
+
+  return containerDiv;
+}
 
