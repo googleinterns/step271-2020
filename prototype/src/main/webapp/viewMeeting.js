72490eddf42f4e0594efeb3a60ccac1176be1770
Anna Giang <annagiang@google.com>
2021-01-11 14:22:05 +1100

    Rename PROPOSED_MEETING_TIMES to 'proposedMeetingTimes'
    Variable was not a constant, since it would be incremented.

--- prototype/src/main/webapp/viewMeeting.js
+++ prototype/src/main/webapp/viewMeeting.js
@@ -1,10 +1,10 @@
-PROPOSED_TIMES_NO = 3; // Initially we hardcoded 3 different times suggestions
+let proposedMeetingTimes = 3; // Initially we hardcoded 3 different times suggestions
 
 function voteTime() {
   // get the answers from the form and update votes on screen
   // NOTE this data would be sent to a servlet in MVP and the data fetched to update UI
   let inputs = document.getElementById("vote-time").elements;
-  for (let i = 0; i < PROPOSED_TIMES_NO; i++) {
+  for (let i = 0; i < proposedMeetingTimes; i++) {
     if (inputs[i].checked === true) {
       document.getElementById(inputs[i].id + "-vote").innerText = (
         parseInt(document.getElementById(inputs[i].id + "-vote").innerText) + 1
@@ -16,24 +16,24 @@ function voteTime() {
 }
 
 function proposeTime() {
-  PROPOSED_TIMES_NO++;
+  proposedMeetingTimes++;
 
-  let newTime = document.getElementById("times-table").insertRow(PROPOSED_TIMES_NO);
+  let newTime = document.getElementById("times-table").insertRow(proposedMeetingTimes);
 
   let optionNumber = newTime.insertCell(0);
-  optionNumber.innerText = PROPOSED_TIMES_NO;
+  optionNumber.innerText = proposedMeetingTimes;
 
   let datetime = newTime.insertCell(1);
   datetime.innerText = document.getElementById("new-time").value; // unformatted datestring
 
   let voteCount = newTime.insertCell(2);
   voteCount.innerText = "0";
-  voteCount.id = "time-" + PROPOSED_TIMES_NO + "-vote";
+  voteCount.id = "time-" + proposedMeetingTimes + "-vote";
 
   let voteButtonTh = newTime.insertCell(3);
   let voteButton = document.createElement("input");
   voteButton.type = "radio";
   voteButton.name = "vote-time";
-  voteButton.id = "time-" + PROPOSED_TIMES_NO;
+  voteButton.id = "time-" + proposedMeetingTimes;
   voteButtonTh.appendChild(voteButton);
 }
