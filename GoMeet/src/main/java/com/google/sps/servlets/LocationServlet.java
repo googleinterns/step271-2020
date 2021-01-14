273a5892b9fa9d35b125a66631d32571bda2ce87
Therese Nieva <theresenieva@gmail.com>
2021-01-14 05:27:37 +0000

    Add LocationServlet.java to save and fetch location data.

---
+++ GoMeet/src/main/java/com/google/sps/servlets/LocationServlet.java
@@ -0,0 +1,79 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.sps.data.Location;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/** Handles fetching and saving markers data. */
+@WebServlet("/locations")
+public class LocationServlet extends HttpServlet {
+
+  /** Responds with a JSON array containing marker data. */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+
+    Collection<Location> locations = getLocations();
+    Gson gson = new Gson();
+    String json = gson.toJson(locations);
+
+    response.getWriter().println(json);
+  }
+
+  /** Accepts a POST request containing a new marker. */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    double lat = Double.parseDouble(request.getParameter("lat"));
+    double lng = Double.parseDouble(request.getParameter("lng"));
+    String note = Jsoup.clean(request.getParameter("note"), Whitelist.none());
+    String title = Jsoup.clean(request.getParameter("title"), Whitelist.none());
+
+    Location location = new Location(title, lat, lng, note);
+    storeLocation(location);
+  }
+
+  /** Fetches markers from Datastore. */
+  private Collection<Location> getLocations() {
+    Collection<Location> locations = new ArrayList<>();
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("Location");
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      String title = (String) entity.getProperty("title");
+      double lat = (double) entity.getProperty("lat");
+      double lng = (double) entity.getProperty("lng");
+      String note = (String) entity.getProperty("note");
+
+      Location location = new Location(title, lat, lng, note);
+      locations.add(location);
+    }
+    return locations;
+  }
+
+  /** Stores a marker in Datastore. */
+  public void storeLocation(Location location) {
+    Entity locationEntity = new Entity("Location");
+    locationEntity.setProperty("title", location.getTitle());
+    locationEntity.setProperty("lat", location.getLat());
+    locationEntity.setProperty("lng", location.getLng());
+    locationEntity.setProperty("note", location.getNote());
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(locationEntity);
+  }
+}
