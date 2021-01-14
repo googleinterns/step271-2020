aa9354f091ea15a2161a43b0580e49a6b0326a8b
Vivian Ho <vivianho18@outlook.com>
2021-01-14 00:14:34 +0000

    Create js file with functions to add and remove guests

---
+++ GoMeet/src/main/webapp/createMeetingStep1.js
@@ -0,0 +1,35 @@
+/**
+ * Adds a meeting guest to ul element with id "meeting-guest"
+ */
+function addMeetingGuest() {
+  let guestName = document.getElementById('meeting-guest').value;
+  
+  // Do not let submission of empty field
+  if (guestName === '') {
+    alert('Please enter a guest name');
+    return;
+  }
+  
+  let guest = document.createElement('li');
+  guest.id = guestName;
+  guest.innerText = guestName;
+
+  // Create delete button associated with this guest
+  let removeGuestButton = document.createElement('button');
+  removeGuestButton.innerHTML = 'X';
+  removeGuestButton.addEventListener('click', function() {removeMeetingGuest(guestName);});
+  
+  guest.appendChild(removeGuestButton);
+  document.getElementById('guest-list').appendChild(guest);
+
+  // Clear text box
+  document.getElementById('meeting-guest').value = '';
+}
+
+/**
+ * Deletes a guest name 'li' element from the page with the id in guest param.
+ * @param {*} guest the id of the 'li' element to be removed.
+ */
+function removeMeetingGuest(guest) {
+  document.getElementById(guest).remove();
+}
