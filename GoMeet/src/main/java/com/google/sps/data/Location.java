52f940135ab0a3e503119bad5e2b18bda1211709
Therese Nieva <theresenieva@gmail.com>
2021-01-14 03:12:40 +0000

    Create Location.java to store venue information.

---
+++ GoMeet/src/main/java/com/google/sps/data/Location.java
@@ -0,0 +1,45 @@
+/** Represents a marker on the map. */
+package com.google.sps.data;
+import java.util.Collection;
+
+public class Location {
+
+  private final String title;
+  private final double lat;
+  private final double lng;
+  private final String note;
+  private int voteCount;
+  private Collection<String> voters;
+
+  public Location(String title, double lat, double lng, String note) {
+    this.title = title;
+    this.lat = lat;
+	this.lng = lng;
+    this.note = note;
+    this.voteCount = 1;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public double getLat() {
+    return lat;
+  }
+
+  public double getLng() {
+    return lng;
+  }
+
+  public String getNote() {
+    return note;
+  }
+
+  public int getVoteCount() {
+    return voteCount;
+  }
+
+  public Collection<String> getVoters() {
+    return voters;
+  }
+}
