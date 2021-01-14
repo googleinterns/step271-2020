d6f740a2304a77cda66d86e42a49de17b5cb9726
Therese Nieva <theresenieva@gmail.com>
2021-01-14 06:44:04 +0000

    Create textbox attached to marker on click to input location details.

--- GoMeet/src/main/webapp/js/view-meeting-location.js
+++ GoMeet/src/main/webapp/js/view-meeting-location.js
@@ -8,4 +8,75 @@ function createMap() {
   map = new google.maps.Map(
       document.getElementById('map'),
       {center: {lat: -33.8688, lng: 151.2093}, zoom: 12}); 
+
+  map.addListener('click', (event) => {
+    createLocationForEdit(event.latLng.lat(), event.latLng.lng());
+  }); 
+}
+
+/** Sends a marker to the backend for saving. */
+function postLocation(title, lat, lng, note) {
+  const params = new URLSearchParams();
+  params.append('title', title);
+  params.append('lat', lat);
+  params.append('lng', lng);
+  params.append('note', note);
+
+  fetch('/locations', {method: 'POST', body: params});
+}
+
+/** Creates a marker that shows a textbox the user can edit. */
+function createLocationForEdit(lat, lng) {
+  // If we're already showing an editable marker, then remove it.
+  if (editLocation) {
+    editLocation.setMap(null);
+  }
+
+  editLocation =
+      new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+
+  const infoWindow =
+      new google.maps.InfoWindow({content: buildInfoWindowInput(lat, lng)});
+
+  // When the user closes the editable info window, remove the marker.
+  google.maps.event.addListener(infoWindow, 'closeclick', () => {
+    editLocation.setMap(null);
+  });
+
+  infoWindow.open(map, editLocation);
+}
+
+/**
+ * Builds and returns HTML elements that show an editable textbox and a submit
+ * button.
+ */
+function buildInfoWindowInput(lat, lng) {
+  const titlePrompt = document.createElement('span');
+  titlePrompt.innerText = "Enter location title:"
+  const titleTextbox = document.createElement('textarea');
+
+  const notePrompt = document.createElement('span');
+  notePrompt.innerText = "Enter note:"
+  const noteTextbox = document.createElement('textarea');
+
+  const button = document.createElement('button');
+  button.appendChild(document.createTextNode('CONFIRM'));
+
+  button.onclick = () => {
+    postLocation(titleTextbox.value, lat, lng, noteTextbox.value);
+    editLocation.setMap(null);
+  };
+
+  const containerDiv = document.createElement('div');
+  containerDiv.appendChild(titlePrompt);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(titleTextbox);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(notePrompt);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(noteTextbox);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(button);
+
+  return containerDiv;
 }
