95f568f852f3d7758530577f63528b3c36c6320d
Anna Giang <annagiang@google.com>
2021-01-18 13:45:28 +1100

    Add corresponding delete buttons for each time input
    The delete buttons will delete the particular input field that they have been associated with.
    Delete buttons will toggle between showing and hidden - they will only be shown when there are at least 2 input fields, and will be hidden when there is only one input field left on the page.

--- GoMeet/src/main/webapp/js/manualTimeProposal.js
+++ GoMeet/src/main/webapp/js/manualTimeProposal.js
@@ -2,21 +2,24 @@ const MAX_TIME_INPUTS = 5; // Allow up to 5 proposed times
 var timeInputs = 1; // Current number of time inputs on page
 const MEETING_TIME_NAME_PREFIX = 'meeting-time-'; // The prefix of the sessionStorage key name for meeting times
 var timeNameSuffix = 1; // The suffix appended to MEETING_TIME_NAME_PREFIX to make the name unique
-
+const MAX_TIME_INPUT_REACHED_ALERT = 'Maximum number of proposed times: ' + MAX_TIME_INPUTS.toString();
 /**
- * Adds a time input field to the page in the meeting-time-inputs div.
+ * Adds a time input field and corresponding delete button to the meeting-time-inputs div.
  */
 function addTimeInput() {
   if (timeInputs >= MAX_TIME_INPUTS) {
-    alert('Maximum number of proposed times: ' + MAX_TIME_INPUTS.toString());
+    alert(MAX_TIME_INPUT_REACHED_ALERT);
     return;
   }
+
   timeInputs++
+  // Show delete buttons when it has reached 2 time inputs on the page
+  if (timeInputs === 2) {
+    toggleDeleteButtons();
+  }
   timeNameSuffix++
   
   let inputDiv = document.getElementById('meeting-time-inputs');
-  let enclosingDiv = document.createElement('div');
-
   let inputField = document.createElement('input');
   inputField.type = 'datetime-local';
   inputField.name = MEETING_TIME_NAME_PREFIX + timeNameSuffix.toString();
@@ -24,8 +27,60 @@ function addTimeInput() {
   let label = document.createElement('label');
   label.htmlFor = inputField.name;
   label.innerText = 'New Time';
+
+  let deleteButton = document.createElement('button');
+  deleteButton.innerText = '-';
+  deleteButton.className = 'delete-time-button';
+  deleteButton.onclick = function() {deleteTimeInput(inputField.name);}; 
   
+  let enclosingDiv = document.createElement('div');
+  enclosingDiv.id = inputField.name; // ID of div = name of input field, to faciliate deleting
+
   enclosingDiv.appendChild(label);
   enclosingDiv.appendChild(inputField);
+  enclosingDiv.appendChild(deleteButton);
   inputDiv.appendChild(enclosingDiv);
+}
+
+/**
+ * Deletes the input div which encloses the datetime-input <input> element, the 
+ * <label> for the <input> and the '-' delete button.
+ * @param {string} inputDivId the id of the div to delete. If invalid or not provided,
+ * this function will do nothing.
+ */
+function deleteTimeInput(inputDivId) {
+  if (inputDivId === null) { // No input ID given
+    return;
+  }
+  let inputDiv = document.getElementById(inputDivId);
+  if (inputDiv == null) { // Input ID is invalid (e.g. does not exist)
+    return;
+  }
+  inputDiv.remove();
+  timeInputs--; 
+  // Hide delete buttons if they only have one time input left
+  if (timeInputs === 1) {
+    toggleDeleteButtons();
+  }
+}
+
+/**
+ * Hides or shows the buttons with className 'delete-time-button'.
+ * Shows buttons if they're hidden, and vice-versa.
+ * Hides buttons by setting buttonElement.style.display to 'none', 
+ * and shows the buttons by setting buttonElement.style.display to
+ * 'inline'.
+ * If no buttons on the page were found with className 'delete-time-button',
+ * this function will do nothing.
+ */
+function toggleDeleteButtons() {
+  let deleteButtons = document.getElementsByClassName('delete-time-button');
+  for (let i = 0; i < deleteButtons.length; i++) {
+    if (deleteButtons[i].style.display === 'none') {
+      deleteButtons[i].style.display = 'inline';
+    }
+    else {
+      deleteButtons[i].style.display = 'none';
+    }
+  }
 }
