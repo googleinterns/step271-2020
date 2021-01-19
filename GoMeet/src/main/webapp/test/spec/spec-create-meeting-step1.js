e4c0854953a4c8b106bccb7acf2153ec2c658825
Vivian Ho <vivianho18@outlook.com>
2021-01-19 22:55:34 +0000

    Fix file naming and variable declaration to match Google Javascript style guide

--- GoMeet/src/main/webapp/test/spec/spec-create-meeting-step1.js
+++ GoMeet/src/main/webapp/test/spec/spec-create-meeting-step1.js
@@ -9,33 +9,33 @@ describe ("Add guest function", function() {
   });
 
   it ("Should add one li element to the html page", function() {
-    var listSize = document.getElementById('guest-list').childNodes.length; 
-    var elementType = document.getElementById('guest-1').tagName;
+    let listSize = document.getElementById('guest-list').childNodes.length; 
+    let elementType = document.getElementById('guest-1').tagName;
     expect(listSize).toBe(1); 
     expect(elementType).toBe("LI"); 
   });
 
   it ("Should add a li element with 3 child nodes", function() {
-    var listElement = document.getElementById('guest-1'); 
-    var numChildren = listElement.childNodes.length; 
+    let listElement = document.getElementById('guest-1'); 
+    let numChildren = listElement.childNodes.length; 
     expect(numChildren).toBe(3); 
   });
 
   it ("The first child node should be a label element", function() {
-    var children = document.getElementById('guest-1').childNodes; 
-    var elementType = children[0].tagName;
+    let children = document.getElementById('guest-1').childNodes; 
+    let elementType = children[0].tagName;
     expect(elementType).toBe("LABEL"); 
   });
 
   it ("The second child node should be an input element", function() {
-    var children = document.getElementById('guest-1').childNodes; 
-    var elementType = children[1].tagName; 
+    let children = document.getElementById('guest-1').childNodes; 
+    let elementType = children[1].tagName; 
     expect(elementType).toBe("INPUT");
   });
 
   it ("The third child node should be a button element", function() {
-    var children = document.getElementById('guest-1').childNodes; 
-    var elementType = children[2].tagName; 
+    let children = document.getElementById('guest-1').childNodes; 
+    let elementType = children[2].tagName; 
     expect(elementType).toBe("BUTTON");
   });
 });
@@ -44,23 +44,23 @@ describe ("Remove guest function", function() {
 
   it ("Should remove one li element from the guest list", function() {
     addGuest(document);
-    var guestList = document.getElementById('guest-list').childNodes;
-    var firstGuestRemoveButton = guestList[0].childNodes[2]; 
+    let guestList = document.getElementById('guest-list').childNodes;
+    let firstGuestRemoveButton = guestList[0].childNodes[2]; 
     firstGuestRemoveButton.click();
-    var listSize = document.getElementById('guest-list').childNodes.length; 
+    let listSize = document.getElementById('guest-list').childNodes.length; 
     expect(listSize).toBe(0); 
   });
 
   it ("Should remove the specific li element that the remove button is a child node of", function() {
     addGuest(document); 
     addGuest(document); 
-    var guestOne = document.getElementById('guest-1'); 
-    var guestTwo = document.getElementById('guest-2');
-    var guestList = document.getElementById('guest-list').childNodes;
+    let guestOne = document.getElementById('guest-1'); 
+    let guestTwo = document.getElementById('guest-2');
+    let guestList = document.getElementById('guest-list').childNodes;
     
     // Remove the second guest 
     guestList[1].childNodes[2].click();
-    var numGuests = guestList.length; 
+    let numGuests = guestList.length; 
     
     expect(guestOne).toBeDefined(); 
     expect(guestTwo).toBeNull;
