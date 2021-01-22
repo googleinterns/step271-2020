2acf3cf03dbbbf7b39be9a77f587a100e3de521c
Therese Nieva <theresenieva@gmail.com>
2021-01-22 02:28:33 +0000

    Create Location object to store data about a meeting venue.

---
+++ GoMeet/src/main/java/com/google/sps/data/Location.java
@@ -0,0 +1,50 @@
+package main.java.com.google.sps.data;
+import java.util.Collection;
+import java.util.HashSet;
+
+/** Represents a possible venue for a meeting. */
+public class Location {
+  private final String title;
+  private final double lat;
+  private final double lng;
+  private final String note;
+  private int voteCount;
+  private Collection<String> voters;
+
+  public Location(String title, double lat, double lng, String note) {
+    this.title = title;
+    this.lat = lat;
+    this.lng = lng;
+    this.note = note;
+    this.voteCount = 1;
+    voters = new HashSet<>();
+  }
+
+  public void addVoter(String voter) {
+    voters.add(voter);
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public double getLat() {
+    return lat;
+  }
+
+  public double getLng() {
+    return lng;
+  }
+
+  public String getNote() {
+    return note;
+  }
+
+  public int getVoteCount() {
+    return voteCount;
+  }
+
+  public Collection<String> getVoters() {
+    return voters;
+  }
+}
