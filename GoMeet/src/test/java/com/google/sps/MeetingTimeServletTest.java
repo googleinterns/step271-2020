703600d6226507ac22a8e7e61052ea16e6e681ad
Anna Giang <annagiang@google.com>
2021-01-24 16:45:49 +1100

    MeetingTimeServlet doGet function and tests
    Added ServletUtil class to contain utility methods that will be shared by many servlets.

--- GoMeet/src/test/java/com/google/sps/MeetingTimeServletTest.java
+++ GoMeet/src/test/java/com/google/sps/MeetingTimeServletTest.java
@@ -13,16 +13,23 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.sps.data.ErrorMessages;
 import com.google.sps.servlets.MeetingTimeServlet;
 import com.google.sps.data.MeetingTimeFields;
+import com.google.sps.data.ServletUtil;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TooManyListenersException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -39,7 +46,10 @@ import org.junit.Test;
 @RunWith(JUnit4.class)
 public final class MeetingTimeServletTest {
   // Hardcoded data
+  private final String MEETING_TIME_ID_VAL = "abc123def456";
+  private final int VOTE_COUNT_VAL = 2;
   private final String DATETIME_VAL = "2021-01-20T16:33:00";
+  private final List VOTERS_VAL = new ArrayList<String>(Arrays.asList("John Smith", "Bob Citizen"));
 
   // Mocks
   private final LocalServiceTestHelper helper =
@@ -89,7 +99,6 @@ public final class MeetingTimeServletTest {
     // check that error response was sent with appropriate details
     verify(mockedResponse, times(1)).sendError(HttpServletResponse.SC_BAD_REQUEST, ErrorMessages.BAD_REQUEST_ERROR);
 
-    // Check that nothing was stored
     List<Entity> results = getAllEntities();
     assertEquals(0, results.size());
 
@@ -98,6 +107,94 @@ public final class MeetingTimeServletTest {
     assertTrue(stringWriter.toString().contains(""));
   }
 
+  // Tests for doGet
+  @Test 
+  public void testSuccessfulDoGet() throws IOException {
+    // hardcode an entity in local datastore
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity fakeMeetingTime = new Entity("MeetingTime");
+    String fakeMeetingTimeKey = fakeMeetingTime.getKey().toString();
+    fakeMeetingTime.setProperty(MeetingTimeFields.MEETING_TIME_ID, fakeMeetingTimeKey);
+    fakeMeetingTime.setProperty(MeetingTimeFields.DATETIME, DATETIME_VAL);
+    fakeMeetingTime.setProperty(MeetingTimeFields.VOTE_COUNT, VOTE_COUNT_VAL);
+    fakeMeetingTime.setProperty(MeetingTimeFields.VOTERS, VOTERS_VAL);
+    datastore.put(fakeMeetingTime); // stores to local datastore
+
+    // add all data to hashmap, and convert to JSON
+    HashMap<String, Object> fakeMeetingTimeMap = new HashMap<String, Object>() {{
+      put(MeetingTimeFields.DATETIME, DATETIME_VAL);
+      put(MeetingTimeFields.VOTE_COUNT, VOTE_COUNT_VAL);
+      put(MeetingTimeFields.VOTERS, VOTERS_VAL);
+    }};
+    String fakeMeetingTimeJson = ServletUtil.convertToJson(fakeMeetingTimeMap);
+
+    // fetch the entity
+    when(mockedRequest.getParameter(MeetingTimeFields.MEETING_TIME_ID))
+        .thenReturn(fakeMeetingTimeKey); 
+    new MeetingTimeServlet().doGet(mockedRequest, mockedResponse);
+
+    // expect the JSON to be returned & no calls to response.sendError
+    writer.flush();
+    assertTrue(stringWriter.toString().contains(fakeMeetingTimeJson));
+    verify(mockedResponse, never()).sendError(anyInt(), anyString());
+  }
+   
+  @Test 
+  public void testDoGetNullKey() throws IOException {
+    when(mockedRequest.getParameter(MeetingTimeFields.MEETING_TIME_ID))
+        .thenReturn(null); // null key
+    new MeetingTimeServlet().doGet(mockedRequest, mockedResponse);
+    
+    // check that error response was sent with appropriate details
+    verify(mockedResponse, times(1))
+        .sendError(HttpServletResponse.SC_BAD_REQUEST, ErrorMessages.BAD_REQUEST_ERROR);
+
+    // Assert that nothing is returned
+    writer.flush(); // writer may not have been flushed yet
+    assertTrue(stringWriter.toString().contains(""));
+  }
+  
+  @Test 
+  public void testDoGetMultipleEntitiesReturned() throws IOException {
+    // hardcode two entities with identical MEETING_TIME_ID in local datastore
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity fakeMeetingTime1 = new Entity("MeetingTime");
+    fakeMeetingTime1.setProperty(MeetingTimeFields.MEETING_TIME_ID, MEETING_TIME_ID_VAL);
+    datastore.put(fakeMeetingTime1); // stores to local datastore
+    Entity fakeMeetingTime2 = new Entity("MeetingTime");
+    fakeMeetingTime2.setProperty(MeetingTimeFields.MEETING_TIME_ID, MEETING_TIME_ID_VAL);
+    datastore.put(fakeMeetingTime2);
+
+    // fetch the entity
+    when(mockedRequest.getParameter(MeetingTimeFields.MEETING_TIME_ID)).thenReturn(MEETING_TIME_ID_VAL); 
+    new MeetingTimeServlet().doGet(mockedRequest, mockedResponse);
+<<<<<<< HEAD
+    TooManyResultsException exception = new TooManyResultsException();
+=======
+    
+>>>>>>> 198a789 (remove instantiation of unused exception)
+    // check that error response was sent with appropriate details
+    verify(mockedResponse, times(1))
+        .sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE, ErrorMessages.TOO_MANY_RESULTS_ERROR);
+
+    // Assert that nothing is returned
+    writer.flush(); // writer may not have been flushed yet
+    assertTrue(stringWriter.toString().contains(""));
+  }
+  
+  @Test 
+  public void testDoGetNoResults() throws IOException {
+    when(mockedRequest.getParameter(MeetingTimeFields.MEETING_TIME_ID)).thenReturn("non-existent key");
+    new MeetingTimeServlet().doGet(mockedRequest, mockedResponse);
+    
+    // check that error response was sent with appropriate details
+    verify(mockedResponse, times(1))
+        .sendError(HttpServletResponse.SC_NOT_FOUND, ErrorMessages.ENTITY_NOT_FOUND_ERROR);
+
+    // Assert that nothing is returned
+    writer.flush(); // writer may not have been flushed yet
+    assertTrue(stringWriter.toString().contains(""));
+  }
   @After
   public void tearDown() {
     helper.tearDown();
