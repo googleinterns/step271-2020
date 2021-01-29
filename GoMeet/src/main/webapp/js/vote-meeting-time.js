12e69a93c33fa2e59ca8b7fa50b193381c0bd698
Anna Giang <annagiang@google.com>
2021-01-29 11:54:47 +1100

    Implementation for generateVoteTimeForm
    This function will be called by fetchAndProcess(meetingTimeIds) to dynamically generate the voting time form given the data passed to it.
    Added function voteTime that will update the votecount data in the backend via the VoteMeetingTimeDAO, when the user clicks button to vote for the time.

--- GoMeet/src/main/webapp/js/vote-meeting-time.js
+++ GoMeet/src/main/webapp/js/vote-meeting-time.js
@@ -72,4 +72,75 @@ async function fetchAndProcess(meetingTimeIds) {
  * {meetingTimeId: id, datetime: datetimeStr, voteCount: count, voters: [voters]}
  */
 function generateVoteTimeForm(timeData, currentUser, votedTimes) {
+  let votingRight = true;
+  if (votedTimes.size === MAX_VOTES) {
+    votingRight = false; // user may not vote again if they have voted maximum times
+  }
+  // disable all vote buttons for users that have voted MAX_VOTES already
+  let disabledState = !votingRight; 
+
+  sortTimesbyVotes(timeData);
+
+  let voteTable = document.getElementById('meeting-times-table');
+  
+  for (let i = 0; i < timeData.length; i++) {
+    let time = timeData[i];
+    let timeRow = voteTable.insertRow(i + 1);
+
+    let dateTimeCell = timeRow.insertCell(0);
+    dateTimeCell.innerText = time.datetime;
+
+    let voteCountCell = timeRow.insertCell(1);
+    voteCountCell.innerText = time.voteCount;
+
+    let votersCell = timeRow.insertCell(2);
+    votersCell.innerText = time.voters;
+
+    // Clicking on this button will cast a vote for this meeting time
+    let voteFormCell = timeRow.insertCell(3);
+    let voteButton = document.createElement('button');
+    voteButton.id = time.id;
+    voteButton.onclick = function() {voteTime(time.id, currentUser)};
+    if (votedTimes.has(time.id)) {
+      // always disable buttons of times the user has voted for already
+      voteButton.disabled = true;
+    } else {
+      voteButton.disabled = disabledState;
+    }
+    voteFormCell.appendChild(voteButton);
+  }
+}
+
+/**
+ * Sorts the timeData objects given by their 'voteCount'
+ * property in descending order
+ * @param {Array[Object]} timeData an array of timeData objects
+ * retrieved from the '/meeting-time' servlet via GET request, in 
+ * the format:
+ * {meetingTimeId: id, datetime: datetimeStr, voteCount: count, voters: [voters]}
+ */
+function sortTimesbyVotes(timeData) {
+  // sort the times by votes, in decreasing order
+  timeData.sort((time1, time2) => {
+    if (time1.voteCount > time2.voteCount) {
+      return -1;
+    }
+    if (time1.voteCount < time2.voteCount) {
+      return 1;
+    }
+    // voteCounts equal
+    return 0;
+  });
+}
+
+/**
+ * Increments the vote count for the MeetingTime with the id
+ * given. Stores the new vote count, and the email of the user 
+ * that voted to Datastore.
+ * @param {String} id The id of the meeting time voted for
+ * @param {String} currentUser The user that is currently logged in,
+ * and who is voting
+ */
+async function voteTime(id, currentUser) {
+  await VoteMeetingTimeDAO.voteMeetingTime(id, currentUser);
 }
--- GoMeet/src/main/webapp/test/spec/spec-vote-meeting-time.js
+++ GoMeet/src/main/webapp/test/spec/spec-vote-meeting-time.js
@@ -110,3 +110,185 @@ describe('fetchAndProcess(meetingTimeIds)', function() {
     expect(window.generateVoteTimeForm).toHaveBeenCalledWith(MEETING_TIME_DATA, CURRENT_USER, VOTED_TIMES);
   });
 });
+
+// TESTS FOR generateVoteTimeForm
+/**
+ * Verifies that the cells of each row of the meeting time voting table 
+ * contains expected data (in order: datetime, voteCount, 
+ * voters list, voting button).
+ */
+function verifyRowData() {
+  // there should be 4 fields of information per row: 
+  // datetime, voteCount, voters, voting button
+  const ROW_LENGTH = 4; 
+  let table = document.getElementById('meeting-times-table');
+  expect(table.rows.length)
+      .toEqual(MEETING_TIME_DATA.length + 1); // length of data + 1 to include headers row
+  
+  for (let i = 1; i < table.rows.length; i++) {
+    let dataRow = table.rows.item(i);
+    expect(dataRow.children.length).toEqual(ROW_LENGTH);
+    // first cell in the datetime string
+    expect(dataRow.children.item(0).innerText)
+        .toEqual(MEETING_TIME_DATA[i - 1].datetime); 
+    // second cell is the voteCount
+    expect(dataRow.children.item(1).innerText)
+        .toEqual(MEETING_TIME_DATA[i - 1].voteCount.toString());
+    // third cell is the voters 
+    expect(dataRow.children.item(2).innerText)
+        .toEqual(MEETING_TIME_DATA[i - 1].voters.toString());
+    // fourth cell contains the voting button
+    let voteButton = dataRow.children.item(3).children.item(0);
+    expect(voteButton.tagName).toEqual('BUTTON');
+    expect(voteButton.onclick.toString()).toEqual(`function() {voteTime(time.id, currentUser)}`);
+  }
+}
+
+describe('generateVoteTimeForm', function() {
+  it('creates rows in the table with id="meeting-times-table" with the data received \
+      disabling all the vote buttons if the user has voted MAX_VOTES times', async function() {
+    generateVoteTimeForm(MEETING_TIME_DATA, CURRENT_USER, VOTED_TIMES);
+    verifyRowData();
+    // verify the voting buttons separately, as their state varies depending on voter
+    let table = document.getElementById('meeting-times-table');
+    for (let i = 1; i < table.rows.length; i++) {
+      let dataRow = table.rows.item(i);
+      let voteButton = dataRow.children.item(3).children.item(0);
+      expect(voteButton.disabled).toBe(true); // currentUser has voted for MAX_VOTES times
+    }
+  });
+
+  it('creates rows in the table with id="meeting-times-table" with the data received \
+      disabling the vote buttons of times that the user has voted for', async function() {
+    let votedTimesTherese = new Set(['def456', 'ghi789']);
+    generateVoteTimeForm(MEETING_TIME_DATA, "therese@test.com", votedTimesTherese);
+    verifyRowData();
+    let table = document.getElementById('meeting-times-table');
+    for (let i = 1; i < table.rows.length; i++) {
+      let dataRow = table.rows.item(i);
+      let voteButton = dataRow.children.item(3).children.item(0);
+      // If the button if associated with a time not voted for yet, 
+      // button should not be disabled
+      if (!votedTimesTherese.has(voteButton.id)) {
+        expect(voteButton.disabled).toBe(false); 
+      } else {
+        expect(voteButton.disabled).toBe(true); 
+      }
+    }
+  });
+
+  it('creates rows in the table with id="meeting-times-table" with the data received \
+      enabling all vote buttons if user has not voted yet', async function() {
+    let votedTimesNewUser = new Set();
+    generateVoteTimeForm(MEETING_TIME_DATA, "newuser@test.com", votedTimesNewUser);
+    verifyRowData();
+    let table = document.getElementById('meeting-times-table');
+    for (let i = 1; i < table.rows.length; i++) {
+      let dataRow = table.rows.item(i);
+      let voteButton = dataRow.children.item(3).children.item(0);
+      expect(voteButton.disabled).toBe(false); // currentUser has not voted at all
+    }
+  });
+  afterEach(function() {
+    // reset the 'meeting-times-table' table to JUST include the headers
+    let table = document.getElementById('meeting-times-table');
+    for (let i = table.rows.length - 1; i > 0; i--) {
+      table.deleteRow(i);
+    }
+  });
+});
+
+// TESTS FOR sortTimeByVotes
+describe('sortTimeByVotes', function() {
+  let sortedTimeData;
+  beforeEach(function() {
+    sortedTimeData = [
+      {
+        datetime: '2021-01-27T11:20',
+        voteCount: 3,
+        voters: ['anna@test.com', 'therese@test.com', 'vivian@test.com'],
+      },
+      {
+        datetime: '2021-01-28T11:20',
+        voteCount: 2,
+        voters: ['anna@test.com', 'therese@test.com'],
+      },
+      {
+        datetime: '2021-01-28T17:20',
+        voteCount: 1,
+        voters: ['anna@test.com'],
+      }
+    ];
+  });
+
+  it('sorts the timeData objects by voteCount in decreasing order', function() {
+    let timeData = [
+      {
+        datetime: '2021-01-28T17:20',
+        voteCount: 1,
+        voters: ['anna@test.com'],
+      },
+      {
+        datetime: '2021-01-28T11:20',
+        voteCount: 2,
+        voters: ['anna@test.com', 'therese@test.com'],
+      },
+      {
+        datetime: '2021-01-27T11:20',
+        voteCount: 3,
+        voters: ['anna@test.com', 'therese@test.com', 'vivian@test.com'],
+      }
+    ];
+    sortTimesbyVotes(timeData);
+    expect(timeData).toEqual(sortedTimeData);
+  });
+
+  it('does not modify the timeData objects if already sorted', function() {
+    // duplicate of sortedTimeData
+    let sortedTimeDataDuplicate = [
+      {
+        datetime: '2021-01-27T11:20',
+        voteCount: 3,
+        voters: ['anna@test.com', 'therese@test.com', 'vivian@test.com'],
+      },
+      {
+        datetime: '2021-01-28T11:20',
+        voteCount: 2,
+        voters: ['anna@test.com', 'therese@test.com'],
+      },
+      {
+        datetime: '2021-01-28T17:20',
+        voteCount: 1,
+        voters: ['anna@test.com'],
+      }
+    ];
+    sortTimesbyVotes(sortedTimeDataDuplicate);
+    expect(sortedTimeDataDuplicate).toEqual(sortedTimeData);
+  });
+
+  it('does not modify an empty list', function() {
+    let emptyTimeData = [];
+    sortTimesbyVotes(emptyTimeData);
+    expect(emptyTimeData).toEqual([]);
+  });
+});
+
+// TESTS FOR voteTime
+describe('voteTime', function() {
+  beforeAll(function() {
+    spyOn(VoteMeetingTimeDAO, 'voteMeetingTime');
+    spyOn(window, 'displayMeetingTimeForm');
+  });
+
+  it('calls VoteMeetingTimeDAO.voteMeetingTime with the \
+      MeetingTimeId and currentUser', async function() {
+    await voteTime(MEETING_TIME_IDS[0], CURRENT_USER);
+    expect(VoteMeetingTimeDAO.voteMeetingTime)
+        .toHaveBeenCalledWith(MEETING_TIME_IDS[0], CURRENT_USER);
+  });
+
+  it('calls displayMeetingTimeForm to re-render the page', async function() {
+    await voteTime(MEETING_TIME_IDS[0], CURRENT_USER);
+    expect(window.displayMeetingTimeForm).toHaveBeenCalled();
+  });
+});
