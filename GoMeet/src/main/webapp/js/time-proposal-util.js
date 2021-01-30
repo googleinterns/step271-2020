90a6445e7dc66b49dd7ac95d214e60390765857f
Anna Giang <annagiang@google.com>
2021-01-30 16:18:58 +1100

    Refactor utility functions outside of manual-time-proposal.js
    The inputted date verification methods, and the method to get the datetime string of the current time and date will be needed again to verify that the user inputted period-start and period-end times on the create-meeting-step2-gcal.html pages are in the future. Hence restructure and refactor the functions in preparation for this.

---
+++ GoMeet/src/main/webapp/js/time-proposal-util.js
@@ -0,0 +1,65 @@
+class TimeProposalUtil {
+  /**
+   * Returns the ISO datestring of the local current date and time (hours and minutes),
+   * with the time zone data trimmed.
+   * The timezone data in the string returned by Date.toISOString() is always 'Z'
+   * representing UTC time, meaning regardless of what Date() is initialised with,
+   * the time is assumed to be UTC time.
+   * Seconds and milliseconds are set to 0, as that level of granularity is
+   * not required for the purposes of this module.
+   * @returns the ISO string in the format YYYY-MM-DDTHH:MM:00:00
+   */
+  static getDatetimeNow() {
+    let now = new Date();
+
+    // number of minutes BEHIND that UTC time is relative to local time
+    // e.g. Sydney Time is 660 minutes (11 hrs) ahead, so timezoneOffset = -660
+    let timezoneOffset = now.getTimezoneOffset();
+
+    let nowOffset = new Date(now.getTime() - timezoneOffset * 60 * 1000);
+    nowOffset.setMilliseconds(0);
+    nowOffset.setSeconds(0);
+    nowOffset = nowOffset.toISOString();
+    nowOffset = nowOffset.substring(0, nowOffset.length - 1); // trim zone data off
+
+    return nowOffset;
+  }
+
+  /**
+   * Clears the input field and alerts the user, if the user entered a time that
+   * is earlier than or equal to the date and time now. See verifyUniqueFutureTime.
+   * @param {Element} elem the input element where the user inputted the time
+   * @param {Set} enteredTimes The set of datetime strings representing
+   * all the times entered so far. Function will add 'time' to this
+   * set if 'time' is unique and in the future.
+   */
+  static rectifyInputtedTime(elem, enteredTimes) {
+    if (!TimeProposalUtil.verifyUniqueFutureTime(elem.value, enteredTimes)) {
+      elem.value = "";
+      alert(INVALID_TIME_ERROR);
+    }
+  }
+
+  /**
+   * Verify that the time is a future time relative to
+   * the time now, and it is unique (i.e. not in enteredTimes).
+   * If unique and in the future, add to the enteredTimes set, and
+   * return true, otherwise return false.
+   * @param {String} time The datetime string to be verified.
+   * @param {Set} enteredTimes The set of datetime strings representing
+   * all the times entered so far. Function will add 'time' to this
+   * set if 'time' is unique and in the future.
+   * @return true if the time is unique and in the future,
+   * otherwise false
+   */
+  static verifyUniqueFutureTime(time, enteredTimes) {
+    let timeEntered = new Date(time);
+    let now = new Date(TimeProposalUtil.getDatetimeNow());
+    if (enteredTimes.has(time) || !(timeEntered > now)) {
+      return false;
+    } else {
+      enteredTimes.add(time);
+      return true;
+    }
+  }
+}
--- GoMeet/src/main/webapp/test/spec/spec-manual-time-proposal.js
+++ GoMeet/src/main/webapp/test/spec/spec-manual-time-proposal.js
@@ -60,7 +60,7 @@ describe('addTimeInput', function () {
     expect(inputField.type).toEqual('datetime-local');
     expect(inputField.name).toEqual(inputName);
     expect(inputField.min).toEqual(ISO_DATESTRING);
-    expect(inputField.onchange.toString()).toBe('function() {rectifyInputtedTime(this);}');
+    expect(inputField.onchange.toString()).toBe('function() {TimeProposalUtil.rectifyInputtedTime(this, enteredTimes);}');
 
     var deleteButton = addedInputDiv.children.item(2);
     expect(deleteButton.tagName).toEqual('BUTTON');
@@ -252,96 +252,4 @@ describe('toggleDeleteButtons', function () {
   afterEach(function() {
     resetPageState();
   }); 
-});
-
-// Tests for getDatetimeNow
-describe('getDatetimeNow', function() {
-  beforeEach(function() {
-    jasmine.clock().install();
-    jasmine.clock().mockDate(FAKE_NOW);
-  });
-
-  it('returns the ISO datestring of the local date and time, \
-      with seconds and milliseconds set to 0', function() {
-    let now = getDatetimeNow();
-    expect(now).toEqual(ISO_DATESTRING);
-  });
-
-  afterEach(function() {
-    jasmine.clock().uninstall();
-  })
-});
-
-// Tests for verifyUniqueFutureTime
-describe('verifyUniqueFutureTime', function() {
-  beforeEach(function() {
-    jasmine.clock().install();
-    jasmine.clock().mockDate(FAKE_NOW);
-    spyOn(window, 'alert');
-  });
-
-  it('returns false, and does not add the input to the enteredTimes set \
-      if the time entered is earlier than now', function() {
-    // create a date that is 24 hours (24*60*60*1000 milliseconds) earlier than FAKE_NOW
-    let inputtedTime = new Date(FAKE_NOW.getTime() - 24*60*60*1000).toISOString();
-    inputtedTime = inputtedTime.substring(0, inputtedTime.length - 1); // trim the time zone data
-    expect(verifyUniqueFutureTime(inputtedTime)).toBe(false);
-    expect(enteredTimes.has(inputtedTime)).toBe(false);
-  });
-
-  it('returns false and does not add the input to the enteredTimes set, \
-      if the time entered is equal to now', function() {
-    // create a date that is 24 hours (24*60*60*1000 milliseconds) earlier than FAKE_NOW
-    let inputtedTime = new Date(FAKE_NOW.getTime() - 24*60*60*1000).toISOString();
-    inputtedTime = inputtedTime.substring(0, inputtedTime.length - 1);
-    expect(verifyUniqueFutureTime(inputtedTime)).toBe(false);
-    expect(enteredTimes.has(inputtedTime)).toBe(false);
-  });
-
-  it('returns true if the time entered is later than now, \
-      and adds the time to the enteredTimes set', function() {
-    // create a date that is 24 hours (24*60*60*1000 milliseconds) later than FAKE_NOW
-    let inputtedTime = new Date(FAKE_NOW.getTime() + 24*60*60*1000).toISOString();
-    inputtedTime = inputtedTime.substring(0, inputtedTime.length - 1);
-    expect(verifyUniqueFutureTime(inputtedTime)).toBe(true);
-    expect(enteredTimes.has(inputtedTime)).toBe(true);
-  });
-
-  afterEach(function() {
-    jasmine.clock().uninstall();
-    enteredTimes.clear();
-  })
-});
-
-// Tests for rectifyInputtedTime
-describe('rectifyInputtedTime', function() {
-  var inputElem = document.createElement('input');
-  inputElem.type = 'datetime-local';
-
-  beforeEach(function() {
-    spyOn(window, 'alert');
-  });
-
-  it('clears the input field and alerts the user if verifyUniqueFutureTime is false', function() {
-    spyOn(window, 'verifyUniqueFutureTime').and.returnValue(false);
-    // set the value of the inputElem to a date so it's not empty
-    let value = new Date().toISOString();
-    value = value.substring(0, value.length - 1);
-    inputElem.value = value;
-    rectifyInputtedTime(inputElem);
-    // the inputElem value should have been reset
-    expect(inputElem.value).toEqual('');
-    expect(window.alert).toHaveBeenCalledWith(INVALID_TIME_ERROR);
-  });
-
-  it('does not clear the input field if verifyUniqueFutureTime is true', function() {
-    spyOn(window, 'verifyUniqueFutureTime').and.returnValue(true);
-    // set the value of the inputElem to a date so it's not empty
-    let value = new Date().toISOString();
-    value = value.substring(0, value.length - 1);
-    inputElem.value = value;
-    rectifyInputtedTime(inputElem);
-    expect(inputElem.value).toEqual(value); // value should have stayed the same
-    expect(window.alert).not.toHaveBeenCalled();
-  });
 });
