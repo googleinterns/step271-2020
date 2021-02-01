19720ac9d58e257df427b321db7c0fcbeeb65681
Vivian Ho <vivianho18@outlook.com>
2021-02-01 01:50:12 +0000

    Fix nit

--- GoMeet/src/main/webapp/js/meeting-display.js
+++ GoMeet/src/main/webapp/js/meeting-display.js
@@ -5,16 +5,15 @@ function toggleMeetingDisplay() {
     if (loginStatus.loggedIn === 'true') {
       style = 'block'; 
       prompt = '<a href=' + loginStatus.logoutUrl + '>Logout</a>';
-      document.getElementById('login-or-logout-prompt').innerHTML = prompt;
     } else {
       style = 'none'; 
       prompt = '<p>Please log in to view the meeting event details</p>'
       + '<a href=' + loginStatus.loginUrl + '>Login</a>';
-      document.getElementById('login-or-logout-prompt').innerHTML = prompt;
     }
     document.getElementById('meeting-title').style.display = style.valueOf();
     document.getElementById('vote-meeting-times').style.display = style.valueOf();
     document.getElementById('vote-meeting-locations').style.display = style.valueOf();
     document.getElementById('votes-table').style.display = style.valueOf();
+    document.getElementById('login-or-logout-prompt').innerHTML = prompt;
   });
 }
