44f08cedf3bab23b68ed1f0a921f2df4951cdbe2
Vivian Ho <vivianho18@outlook.com>
2021-02-01 00:53:27 +0000

    Fix nit on meeting-display.js

--- GoMeet/src/main/webapp/js/meeting-display.js
+++ GoMeet/src/main/webapp/js/meeting-display.js
@@ -4,22 +4,23 @@ function toggleMeetingDisplay() {
     let meetingTimes = document.getElementById('vote-meeting-times'); 
     let meetingLocations = document.getElementById('vote-meeting-locations'); 
     let votesTable = document.getElementById('votes-table'); 
+    let prompt; 
     
     if (loginStatus.loggedIn === 'true') {
       meetingName.style.display = 'block';
       meetingTimes.style.display = 'block';
       meetingLocations.style.display = 'block';
       votesTable.style.display = 'block';
-      let logoutPrompt = '<a href=' + loginStatus.logoutUrl + '>Logout</a>';
-      document.getElementById('login-or-logout-prompt').innerHTML = logoutPrompt;
+      prompt = '<a href=' + loginStatus.logoutUrl + '>Logout</a>';
+      document.getElementById('login-or-logout-prompt').innerHTML = prompt;
     } else {
       meetingName.style.display = 'none';
       meetingTimes.style.display = 'none';
       meetingLocations.style.display = 'none';
       votesTable.style.display = 'none';
-      let loginPrompt = '<p>Please log in to view the meeting event details</p>'
+      prompt = '<p>Please log in to view the meeting event details</p>'
       + '<a href=' + loginStatus.loginUrl + '>Login</a>';
-      document.getElementById('login-or-logout-prompt').innerHTML = loginPrompt;
+      document.getElementById('login-or-logout-prompt').innerHTML = prompt;
     }
   });
 }
