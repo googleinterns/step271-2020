e03ab7ce7f5caf7fcbba0c051c8f0ed734a00d3c
Therese Nieva <theresenieva@gmail.com>
2021-02-02 01:58:58 +0000

    Remove extra whitespace.

--- GoMeet/src/main/webapp/test/spec/spec-interactive-map.js
+++ GoMeet/src/main/webapp/test/spec/spec-interactive-map.js
@@ -3,7 +3,7 @@ describe('Create map', function() {
   it ('Should create and return a map object', function() {
     const mapConstructorSpy = spyOn(google.maps, 'Map');
     const mockedMap = jasmine.createSpyObj('Map', ['addListener']);
-    mapConstructorSpy.and.returnValue(mockedMap); 
+    mapConstructorSpy.and.returnValue(mockedMap);
 
     const createdMap = createMap();
 
