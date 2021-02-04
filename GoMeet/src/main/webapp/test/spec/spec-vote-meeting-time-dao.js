d69534c51fbddeb33d9a58b767765017bba80926
Anna Giang <annagiang@google.com>
2021-02-04 11:08:06 +1100

    Refactor error response verification to a helper function
    The verification of the error messages in each test case is extremely similar, so extract the code to an external function to reduce repeated code.

--- GoMeet/src/main/webapp/test/spec/spec-vote-meeting-time-dao.js
+++ GoMeet/src/main/webapp/test/spec/spec-vote-meeting-time-dao.js
@@ -1,3 +1,24 @@
+/**
+ * Verfies that the expected error message was returned by VoteMeetingTimeDAO.voteMeetingTime,
+ * given the combination of parameters passed to it.
+ * @param {String} meetingTimeId The meetingTimeId to be passed to VoteMeetingTimeDAO.voteMeetingTime
+ * @param {String} voter The voter to be passes to VoteMeetingTimeDAO.voteMeetingTime
+ * @param {String} expectedError The error message expected from VoteMeetingTimeDAO.voteMeetingTime 
+ * given the meetingTimeId and voter passed to it.
+ */
+async function verifyErrorResponse(meetingTimeId, voter, expectedError) {
+  // Note: cannot use expect().toThrow, because the .toThrow executes before call to
+  // async voteMeetingTime is resolved.
+  let errorMessage;
+  try {
+    await VoteMeetingTimeDAO.voteMeetingTime(meetingTimeId, voter);
+  } catch (error) {
+    errorMessage = error.message;
+  }
+  expect(errorMessage).toEqual(expectedError);
+}
+
+// TESTS FOR VoteMeetingTimeDAO.voteMeetingTime()
 describe('VoteMeetingTimeDAO - voteMeetingTime', function () {
   const MEETING_TIME_ID = 'abc123def456';
   const NON_EXISTENT_ID = 'non-existent-id';
@@ -34,49 +55,23 @@ describe('VoteMeetingTimeDAO - voteMeetingTime', function () {
   });
 
   it('throws an exception if the meetingTimeId param is not provided', async function () {
-    // Note: cannot use expect().toThrow, because the .toThrow executes before call to
-    // async voteMeetingTime is resolved.
     let meetingTimeId = null;
-    let errorMessage;
-    try {
-      await VoteMeetingTimeDAO.voteMeetingTime(meetingTimeId, VOTER);
-    } catch (error) {
-      errorMessage = error.message;
-    }
-    expect(errorMessage).toEqual(INSUFFICIENT_REQUEST_PARAM);
+    await verifyErrorResponse(meetingTimeId, VOTER, INSUFFICIENT_REQUEST_PARAM);
   });
 
   it('throws an exception if the meetingTimeId param is not of string type', async function () {
     let meetingTimeId = 123;
-    let errorMessage;
-    try {
-      await VoteMeetingTimeDAO.voteMeetingTime(meetingTimeId, VOTER);
-    } catch (error) {
-      errorMessage = error.message;
-    }
-    expect(errorMessage).toEqual(INVALID_PARAM_TYPE);
+    await verifyErrorResponse(meetingTimeId, VOTER, INVALID_PARAM_TYPE);
   });
 
   it('throws an exception if the voter param is not provided', async function () {
     let voter = null;
-    let errorMessage;
-    try {
-      await VoteMeetingTimeDAO.voteMeetingTime(MEETING_TIME_ID, voter);
-    } catch (error) {
-      errorMessage = error.message;
-    }
-    expect(errorMessage).toEqual(INSUFFICIENT_REQUEST_PARAM);
+    await verifyErrorResponse(MEETING_TIME_ID, voter, INSUFFICIENT_REQUEST_PARAM);
   });
 
   it('throws an exception if the voter param is not of string type', async function () {
     let voter = true;
-    let errorMessage;
-    try {
-      await VoteMeetingTimeDAO.voteMeetingTime(MEETING_TIME_ID, voter);
-    } catch (error) {
-      errorMessage = error.message;
-    }
-    expect(errorMessage).toEqual(INVALID_PARAM_TYPE);
+    await verifyErrorResponse(MEETING_TIME_ID, voter, INVALID_PARAM_TYPE);
   });
 
   it('when successful, returns a JSON string with the OK status code (200)', async function () {
