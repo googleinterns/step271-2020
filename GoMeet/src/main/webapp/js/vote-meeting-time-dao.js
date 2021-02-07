a47469e0ca0a3e7d6b3e19a5998a50b6c5e37358
Anna Giang <49330921+anna-giang@users.noreply.github.com>
2021-02-08 09:01:13 +1100

    Vote Meeting Times JavaScript DAO (#32)
    * VoteMeetingTimeDAO voteMeetingTime function and tests
    
    VoteMeetingTimeDAO will be used to access the '/vote-meeting-time' endpoint, where votes for a particular meeting time will be incremented.
    
    * change fetch call in VoteMeetingTimeDao to use fetch().then() structure
    
    * Refactor: move repeated 'url' function to DAOUtils class
    
    The URL function will be used by many DAO to create the URL to pass to the fetch function to send/fetch data from the backend, hence extract out to a shared class.
    
    * Refactor error response verification to a helper function
    
    The verification of the error messages in each test case is extremely similar, so extract the code to an external function to reduce repeated code.
    
    * Update tests to take an email address as the VOTER
    
    Rather than users' names, the application will now use user emails to identify them. Hence the VOTER param passed to VoteMeetingTimeDAO.voteMeetingTime() will be the user email.
    
    * Combine two if-blocks into one
    
    Both if-blocks enclosed the same behaviour, so combine the conditions into one block.
    
    * Fix comment styles to be consistent

---
+++ GoMeet/src/main/webapp/js/vote-meeting-time-dao.js
@@ -0,0 +1,34 @@
+class VoteMeetingTimeDAO {
+  static endpoint = '/vote-meeting-time?'; // Endpoint associated with this servlet.
+
+  /**
+   * Increments the number of votes associated with the MeetingTime
+   * entity as identified by meetingTimeId, and adds the voter to the
+   * collection of people that have voted for this meeting time.
+   * @param {String} meetingTimeId The ID (entity Key) of the MeetingTime
+   * entity to be voted for.
+   * @param {String} voter The name or identifier of the user voting for the
+   * MeetingTime.
+   */
+  static async voteMeetingTime(meetingTimeId, voter) {
+    if (meetingTimeId === null || meetingTimeId === undefined || 
+        voter === null || voter === undefined) {
+      throw new Error(INSUFFICIENT_REQUEST_PARAM);
+    }
+
+    if (typeof meetingTimeId !== 'string' || typeof voter !== 'string') {
+      throw new Error(INVALID_PARAM_TYPE);
+    }
+
+    // Encode the data to be sent in the query string.
+    let urlString = DAOUtils.url(VoteMeetingTimeDAO.endpoint, {
+      'meetingTimeId': meetingTimeId,
+      'voters': voter
+    }); 
+    let responseInit = {
+      method: 'POST'
+    }
+    let response = await fetch(urlString, responseInit).then((response) => response.json());
+    return response;
+  }
+}
