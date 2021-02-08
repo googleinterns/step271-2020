dcceafab12813c61e49e0112ba72a922f215a4c1
Therese Nieva <theresenieva@gmail.com>
2021-02-08 23:01:03 +0000

    Remove unused imports.

--- GoMeet/src/main/java/com/google/sps/dao/LocationDao.java
+++ GoMeet/src/main/java/com/google/sps/dao/LocationDao.java
@@ -8,8 +8,6 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
