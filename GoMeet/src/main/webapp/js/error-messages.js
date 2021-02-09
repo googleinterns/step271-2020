f989d103d9a56eab4f8dcaae08520555da313cd0
Vivian Ho <vivianho18@outlook.com>
2021-02-09 23:29:06 +0000

    Add error message constant

--- GoMeet/src/main/webapp/js/error-messages.js
+++ GoMeet/src/main/webapp/js/error-messages.js
@@ -6,3 +6,4 @@ const INVALID_TIME_ERROR = 'The dates and times entered should be unique and be 
 const INSUFFICIENT_REQUEST_PARAM = 'Please provide all required parameters to the GET request';
 const INVALID_PARAM_TYPE = 'Invalid parameter type';
 const INVALID_PARAM_VALUE = 'Invalid parameter value. Please check that the parameters are of the correct format and range';
+const UNABLE_TO_SUBMIT_MEETING = 'Unable to save meeting event to datastore and retrieve ID';
