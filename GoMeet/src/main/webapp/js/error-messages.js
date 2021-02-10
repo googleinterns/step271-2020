c572f96f3830893e67f394443b37b43d7feec407
Therese Nieva <theresenieva@gmail.com>
2021-02-10 03:22:37 +0000

    Update newLocation for temporary storage to check if the title is unique.

--- GoMeet/src/main/webapp/js/error-messages.js
+++ GoMeet/src/main/webapp/js/error-messages.js
@@ -7,3 +7,5 @@ const INSUFFICIENT_REQUEST_PARAM = 'Please provide all required parameters to th
 const INVALID_PARAM_TYPE = 'Invalid parameter type';
 const INVALID_PARAM_VALUE = 'Invalid parameter value. Please check that the parameters are of the correct format and range';
 const ENTITY_NOT_FOUND = 'This entity could not be found';
+const SAME_TITLE = 'There is already a location with this title';
+const USER_HAS_VOTED = 'You have already voted for this';
