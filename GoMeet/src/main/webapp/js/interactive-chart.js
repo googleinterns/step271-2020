13d91b792cfbaf06e8fe85bff3f9c4698b8d86ce
Therese Nieva <theresenieva@gmail.com>
2021-02-11 08:09:39 +0000

    Add interactive chart to meeting event page to display votes.

---
+++ GoMeet/src/main/webapp/js/interactive-chart.js
@@ -0,0 +1,21 @@
+google.charts.load('current', {'packages':['corechart']});
+
+/** Fetches fruit data and uses it to create a chart. */
+async function drawChart() {
+  const dao = MeetingLocationDaoFactory.getLocationDao('permanent');
+  let locations = await dao.fetchLocations();
+  let data = new google.visualization.DataTable();
+  data.addColumn('string', 'Location');
+  data.addColumn('number', 'Count')
+  locations.forEach((location) => {
+    data.addRow([location.title, location.voteCount])
+  });
+  
+  const options = {
+    'title': 'Locations',
+    'height': 400
+  };
+
+  const chart = new google.visualization.BarChart(document.getElementById('map'));
+  chart.draw(data, options);
+}
---
+++ GoMeet/src/main/webapp/test/spec/spec-interactive-chart.js
@@ -0,0 +1,28 @@
+/** Tests for interactive-chart.js. */
+describe('Draw Chart', function() {
+  const LOCATION_A = {title : 'Liyue', voteCount : 10};
+  const LOCATIONS = [LOCATION_A];
+
+  it ('Should fetch locations and add data to chart', async function() {
+    // Spy on Dao.
+    const mockedDao = new PermMeetingLocationDAO();
+    spyOn(mockedDao, 'fetchLocations').and.returnValue(LOCATIONS);
+    spyOn(MeetingLocationDaoFactory, 'getLocationDao').and.returnValue(
+        mockedDao);
+
+    // Spy on Charts API.
+    const dataTableSpy = spyOn(google.visualization, 'DataTable');
+    const mockedDataTable =
+        jasmine.createSpyObj('DataTable', ['addColumn', 'addRow']);
+    dataTableSpy.and.returnValue(mockedDataTable);
+    const barChartSpy = spyOn(google.visualization, 'BarChart');
+    const mockedBarChart = jasmine.createSpyObj('BarChart', ['draw']);
+    barChartSpy.and.returnValue(mockedBarChart);
+    spyOn(google.charts, 'load');
+
+    await drawChart();
+
+    expect(mockedDataTable.addRow).toHaveBeenCalledWith(
+        [LOCATION_A.title, LOCATION_A.voteCount]);
+  });
+});
