35f34ea0fc4c4c2d2725429a114393d3511f453d
Therese Nieva <theresenieva@gmail.com>
2021-02-12 00:30:55 +0000

    Update chart test to test if chart.draw() was called correctly.

--- GoMeet/src/main/webapp/test/spec/spec-interactive-chart.js
+++ GoMeet/src/main/webapp/test/spec/spec-interactive-chart.js
@@ -4,6 +4,8 @@ describe('Draw Chart', function() {
   const LOCATIONS = [LOCATION_A];
 
   it ('Should fetch locations and add data to chart', async function() {
+    let passedDataTable;
+
     // Spy on Dao.
     const mockedDao = new PermMeetingLocationDao();
     spyOn(mockedDao, 'fetchLocations').and.returnValue(LOCATIONS);
@@ -17,12 +19,20 @@ describe('Draw Chart', function() {
     dataTableSpy.and.returnValue(mockedDataTable);
     const barChartSpy = spyOn(google.visualization, 'BarChart');
     const mockedBarChart = jasmine.createSpyObj('BarChart', ['draw']);
+    mockedBarChart.draw.and.callFake(function(dataTable, options) {
+      passedDataTable = dataTable;
+    });
     barChartSpy.and.returnValue(mockedBarChart);
     spyOn(google.charts, 'load');
 
     await drawChart();
 
+    // Check that the fetch location data was passed to
+    // google.visualization.dataTable().
     expect(mockedDataTable.addRow).toHaveBeenCalledWith(
         [LOCATION_A.title, LOCATION_A.voteCount]);
+    // Check that the DataTable with the location data was passed
+    // charts.draw();
+    expect(passedDataTable).toBe(mockedDataTable);
   });
 });
