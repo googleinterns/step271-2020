05211174938cb078e5b0e3340182544cc06b381f
Anna Giang <49330921+anna-giang@users.noreply.github.com>
2021-02-15 14:39:09 +1100

    AutoProposeTimes class - Google Calendar Meeting Time Proposal  (#50)
    * Add dependencies for Calendar API to pom file
    
    * AutoProposeTimes class
    
    Class to contain the functions to call the Google Calendar API and to generate the meeting times based on the user data retrieved.
    Note that meetingDuration is in milliseconds as dates and times are compared in milliseconds, so the duration in milliseconds will make it more convenient for comparison and date arithmetic later on.
    
    * Implementation for freebusyRequest() function that calls API
    
    Function will be called to make a request to the Google Calendar API (Freebusy library) to retrieve busy times.
    These times will be used in the algorithm identify when the user is free.
    
    * proposeTimes() function with algorithm to auto-generate meeting times
    
    Propose times function uses the following algorithm to analyse a list of TimePeriods representing busy times:
    1. Sort the list of TimePeriods in order of earliest to latest start time
    2. Scanning over the list of TimePeriod with two  pointers:
        If the end of the current time period (pointed to by left pointer) overlaps with the start of the next time period, move the right pointer one item to the right.
        Otherwise, check the duration of the free time block from the end of the current time period to the start of the next time period.
            If the duration is at least the meeting duration, add to output.
            Move the left pointer to the right pointer (i.e. current is now what the right pointer was pointing to.)
    
    The algorithm time complexity is bounded by the sort of the time periods, O(Nlog(N)), where N is the number of busy TimePeriods in total retrieved from each of the invitees' calendars.
    
    Include the tests for the AutoProposeTime class.
    
    * Refactor API Key into constructor
    
    Rather than requiring calling each of the instance methods of AutoProposeTimes with an API Key, initialise the class with the API Key to avoide repeated code.
    
    * Remove unused imports and list in alphabetical order
    
    * Add timezone information to the dates and times used in algorithm
    
    Previously, the default timezone (UTC time) was being used, and there was no way to select a different time zone for the algorithm to operate in.
    Added support for this functionality, and also set the default timezone to be AET (Australian Eastern Time) to match that of the place the application is being developed at.
    This new functionality will allow also allow client code to have control of what timezone the startTime and endTime are assumed to be in (easier for client code to use this class) and allow it to be sure what timezone the returned data will be in.

---
+++ GoMeet/src/main/java/com/google/sps/data/AutoProposeTimes.java
@@ -0,0 +1,195 @@
+package com.google.sps.data;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar.Freebusy.Query;
+import com.google.api.services.calendar.Calendar.Freebusy;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarRequest;
+import com.google.api.services.calendar.model.FreeBusyCalendar;
+import com.google.api.services.calendar.model.FreeBusyRequest;
+import com.google.api.services.calendar.model.FreeBusyRequestItem;
+import com.google.api.services.calendar.model.FreeBusyResponse;
+import com.google.api.services.calendar.model.TimePeriod;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+public class AutoProposeTimes {
+  private final String RFC3339_FORMAT = "yyyy-MM-dd'T'HH:mm:ssXXX";
+  private static String TIMEZONE = "AET"; // Australian Eastern Time
+
+  Calendar service;
+  String apiKey; 
+  ArrayList<String> calendarId; // The List of Calendar IDs to generate meeting times for
+  DateTime startTime; // The start time of the period to look for possible meeting times
+  DateTime endTime; // The end time of the period to look for possible meeting times
+  int meetingDuration; // The meetingDuration in milliseconds.
+
+   /**
+    * Constructor.
+    * NOTE: Calendar service is dependency injected to allow for easier testing.
+    * @param service The com.google.api.services.calendar.Calendar service to use to 
+    * call APIs from.
+    * @param apiKey The API Key to use the Google Calendar API.
+    * @param calId This list of calendar IDs of calendars to automatically generate
+    * meeting times from.
+    * @param startTime The start of the period in which to search for meeting times.
+    * @param endTime The end of the period in which search for meeting times.
+    * @param duration The duration of the meeting, in milliseconds.
+    */
+  public AutoProposeTimes(Calendar service, String apiKey, ArrayList<String> calId, 
+      Date startTime, Date endTime, int duration) {
+    this.service = service;
+    this.apiKey = apiKey;
+    this.calendarId = calId;
+    this.startTime = new DateTime(startTime, TimeZone.getTimeZone(TIMEZONE));
+    this.endTime = new DateTime(endTime, TimeZone.getTimeZone(TIMEZONE));
+    this.meetingDuration = duration;
+  }
+
+  /**
+   * Used for sorting TimePeriods from earliest start time
+   * to latest start time.
+   */
+  static class TimePeriodComparator implements Comparator<TimePeriod> {
+    public int compare(TimePeriod a, TimePeriod b) { 
+      // Compare the number of milliseconds since the Unix epoch of 
+      // the TimePeriod starts.
+      return (int) (a.getStart().getValue() - b.getStart().getValue()); 
+    } 
+  }
+
+  /**
+   * Generates a List of TimePeriod representing free times with a duration of 
+   * at least the value of 'meetingDuration'.
+   * @return A List of TimePeriod that are free periods where a
+   * meeting may be scheduled.
+   */
+  public List<TimePeriod> proposeTimes() 
+      throws IOException, GoogleJsonResponseException, GeneralSecurityException {
+    // Gather freebusy information for each of the calendars.
+    List<TimePeriod> busyPeriods = new ArrayList<TimePeriod>();
+    List<TimePeriod> freePeriods = new ArrayList<TimePeriod>();
+    for (int i = 0; i < calendarId.size(); i++) {
+      busyPeriods.addAll(freebusyRequest(calendarId.get(i)));
+    }
+
+    // If there are no busyPeriods, add a free time that is the whole period, and return
+    if (busyPeriods.size() == 0) {
+      freePeriods.add(newTimePeriod(this.startTime, this.endTime));
+      return freePeriods;
+    }
+
+    // Sort all the busy periods by start time: O(Nlog(N))
+    Collections.sort(busyPeriods, new TimePeriodComparator());
+
+    long durationStartOfDay;
+    // Add the free time block from startTime to first busy period, if any.
+    DateTime firstBusyPeriod = busyPeriods.get(0).getStart();
+    durationStartOfDay = firstBusyPeriod.getValue() - this.startTime.getValue();
+    if (durationStartOfDay >= this.meetingDuration) {
+      freePeriods.add(newTimePeriod(this.startTime, firstBusyPeriod));
+    }
+
+    // Generate the free periods: O(N)
+    int i = 0;
+    TimePeriod current = busyPeriods.get(i);
+    DateTime lastBusyPeriodEnd = current.getEnd();
+
+    while (i < busyPeriods.size()) {
+      int j = i + 1;
+      while (j < busyPeriods.size() 
+          && current.getEnd().getValue() > 
+              busyPeriods.get(j).getStart().getValue()) {
+        // While the end of the current event overlaps over the start of the next event
+        j++;
+      }
+      if (j == busyPeriods.size()) {
+        break;
+      }
+      // Check that the duration of the free period is at least
+      // of 'meetingDuration' length.
+      DateTime periodStart = current.getEnd(); 
+      DateTime periodEnd = busyPeriods.get(j).getStart();
+      long duration = periodEnd.getValue() - periodStart.getValue();
+      if (duration >= this.meetingDuration) {
+        freePeriods.add(newTimePeriod(periodStart, periodEnd));
+      }
+      // Start from the last TimePeriod we passed in inner loop.
+      i = j;
+      current = busyPeriods.get(i);
+      lastBusyPeriodEnd = current.getEnd();
+    }
+
+    // Add the free time block from end of lastest busy period to endTime, if any.
+    long durationEndOfDay;
+    durationEndOfDay = this.endTime.getValue() - lastBusyPeriodEnd.getValue();
+    if (durationEndOfDay >= this.meetingDuration) {
+      freePeriods.add(newTimePeriod(lastBusyPeriodEnd, this.endTime));
+    }
+
+    return freePeriods;
+  }
+
+  /**
+   * Makes a request to the Freebusy library of the Google Calendar API,
+   * and returns the List of TimePeriods representing busy times of the
+   * user between the startTime and endTime that this instance was 
+   * initialised with.
+   * @param calId The calendar identifier of the calendar to retireve busy
+   * data from.
+   * @return The List of TimePeriods representing busy times in the calendar
+   * identified by the calId.
+   * @throws IOException
+   * @throws GeneralSecurityException
+   */
+  public List<TimePeriod> freebusyRequest(String calId) 
+      throws IOException, GeneralSecurityException {
+    FreeBusyRequestItem currentCalItem = new FreeBusyRequestItem().setId(calId);
+    FreeBusyRequest req = new FreeBusyRequest()
+        .setTimeMin(this.startTime)
+        .setTimeMax(this.endTime)
+        .setItems(Arrays.asList(currentCalItem))
+        .setTimeZone(TIMEZONE); // The timezone that the times returned will be in.
+    Freebusy freebusy = this.service.freebusy();
+    Query query = freebusy.query(req).setKey(this.apiKey);
+    FreeBusyResponse resp = query.execute();
+    // Variable 'currentCal' will store the FreeBusy info of the calendar
+    // with calId as its identifier.
+    FreeBusyCalendar currentCal = resp.getCalendars().get(calId); 
+    return currentCal.getBusy();
+  }
+
+  /**
+   * Sets the timezone that the algorithm will be applying to times.
+   * Timezone is AET (Australian Eastern Time) by default.
+   * startTime and endTime provided to constructor will be assumed to be
+   * this timezone, all auto generated times will be returned in this timezone.
+   * @param timezoneString The string that represents the timezone.
+   * Must be one of the timezones as returned by TimeZone.getAvailableIDs()
+   */
+  public static void setTimezone(String timezone) {
+    TIMEZONE = timezone;
+  }
+
+  /**
+   * Creates a new TimePeriod given the start and end DateTimes.
+   * @param start The DateTime with the start date and time details.
+   * @param end The DateTime with the end date and time details.
+   * @return The new TimePeriod created.
+   */
+  private TimePeriod newTimePeriod(DateTime start, DateTime end) {
+    TimePeriod time = new TimePeriod()
+        .setStart(start)
+        .setEnd(end);
+    return time;
+  }
+}
---
+++ GoMeet/src/test/java/com/google/sps/AutoProposeTimesTest.java
@@ -0,0 +1,299 @@
+package test.java.com.google.sps;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.FreeBusyRequest;
+import com.google.api.services.calendar.model.FreeBusyRequestItem;
+import com.google.api.services.calendar.model.TimePeriod;
+import com.google.gson.Gson;
+import com.google.sps.data.AutoProposeTimes;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TimeZone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for AutoProposeTimes class */
+@RunWith(JUnit4.class)
+public class AutoProposeTimesTest {
+  // Real objects.
+  private Calendar service;
+  private AutoProposeTimes proposer;
+
+  // Spies of the real objects.
+  private Calendar serviceSpy;
+  private AutoProposeTimes proposerSpy;
+
+  // Hardcoded data.
+  private final String FAKE_API_KEY = "key";
+  private final int DURATION_MS = 30 * 60 * 1000; // 30 mins duration in milliseconds.
+  private final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+  private final String TIMEZONE = "AET";
+  private final SimpleDateFormat FORMATTER = new SimpleDateFormat(DATE_FORMAT);
+  private final ArrayList<String> CAL_ID = 
+      new ArrayList<String> (Arrays.asList("person1@test.com", "person2@test.com"));
+  private final String START_TIME_STR = "2021-02-10 10:00:00";
+  private final String END_TIME_STR = "2021-02-11 10:00:00";
+  private final String TIME_10AM = "2021-02-10 10:00:00";
+  private final String TIME_12PM = "2021-02-10 12:00:00";
+  private final String TIME_1230PM = "2021-02-10 12:30:00";
+  private final String TIME_1PM = "2021-02-10 13:00:00";
+  private final String TIME_2PM = "2021-02-10 14:00:00";
+  private final String TIME_4PM = "2021-02-10 16:00:00";
+  private final String TIME_6PM = "2021-02-10 18:00:00"; 
+  private final String TIME_8PM = "2021-02-10 20:00:00"; 
+  private final String TIME_10PM = "2021-02-10 22:00:00"; 
+  private final String TIME_8AM_NEXTDAY = "2021-02-11 08:00:00";
+  private final String TIME_10AM_NEXTDAY = "2021-02-11 10:00:00";
+
+  private Date startTime;
+  private Date endTime;
+
+  @Before
+  public void setup() throws GeneralSecurityException, IOException, ParseException {
+    service = new Calendar.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+        GsonFactory.getDefaultInstance(),
+        null)
+        .setApplicationName("GoMeet")
+        .build();
+    serviceSpy = spy(service);
+    // Initialise the Dates here to recognise the ParseException.
+    startTime = FORMATTER.parse(START_TIME_STR);
+    endTime = FORMATTER.parse(END_TIME_STR);
+    AutoProposeTimes.setTimezone(TIMEZONE);
+    proposer = 
+        new AutoProposeTimes(serviceSpy, FAKE_API_KEY, CAL_ID, startTime, endTime, DURATION_MS);
+    proposerSpy = spy(proposer);
+  }
+
+  /** Tests for proposeTimes */
+
+  // The events of the two guests are separate in the day
+  @Test
+  public void separateBusyPeriods() throws Exception {
+    List<TimePeriod> busyTimesPerson1 = Arrays.asList(
+      timePeriodFromDatestring(TIME_4PM, TIME_6PM),
+      timePeriodFromDatestring(TIME_8AM_NEXTDAY, TIME_10AM_NEXTDAY)
+    );
+    List<TimePeriod> busyTimesPerson2 = Arrays.asList(
+      timePeriodFromDatestring(TIME_12PM, TIME_2PM),
+      timePeriodFromDatestring(TIME_8PM, TIME_10PM)
+    );
+
+    doReturn(busyTimesPerson1).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(0)));
+    doReturn(busyTimesPerson2).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(1)));
+
+    List<TimePeriod> expectedReturn = Arrays.asList(
+      timePeriodFromDatestring(TIME_10AM, TIME_12PM),
+      timePeriodFromDatestring(TIME_2PM, TIME_4PM),
+      timePeriodFromDatestring(TIME_6PM, TIME_8PM),
+      timePeriodFromDatestring(TIME_10PM, TIME_8AM_NEXTDAY)
+    );
+
+    List<TimePeriod> result = proposerSpy.proposeTimes();
+
+    // Assert list sizes are the same
+    assertEquals(expectedReturn.size(), result.size());
+    // Assert that both expected return and actual result are the same
+    assertTrue(result.equals(expectedReturn));
+  }
+
+  // The busy periods of the guests overlap
+  @Test
+  public void overlappingBusyPeriods() throws Exception {
+    List<TimePeriod> busyTimesPerson1 = Arrays.asList(
+      timePeriodFromDatestring(TIME_1PM, TIME_2PM)
+    );
+    List<TimePeriod> busyTimesPerson2 = Arrays.asList(
+      timePeriodFromDatestring(TIME_12PM, TIME_2PM)
+    );
+
+    doReturn(busyTimesPerson1).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(0)));
+    doReturn(busyTimesPerson2).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(1)));
+
+    List<TimePeriod> expectedReturn = Arrays.asList(
+      timePeriodFromDatestring(TIME_10AM, TIME_12PM),
+      timePeriodFromDatestring(TIME_2PM, TIME_10AM_NEXTDAY)
+    );
+
+    List<TimePeriod> result = proposerSpy.proposeTimes();
+
+    // Assert list sizes are the same
+    assertEquals(expectedReturn.size(), result.size());
+    // Assert that both expected return and actual result are the same
+    assertTrue(result.equals(expectedReturn));
+  }
+
+  @Test
+  public void nestedEvents() throws Exception {
+    List<TimePeriod> busyTimesPerson1 = Arrays.asList(
+      // This event is nested in the 12PM-4PM event of person2.
+      timePeriodFromDatestring(TIME_1PM, TIME_2PM),
+      timePeriodFromDatestring(TIME_6PM, TIME_8PM)
+    );
+    List<TimePeriod> busyTimesPerson2 = Arrays.asList(
+      timePeriodFromDatestring(TIME_12PM, TIME_4PM)
+    );
+
+    doReturn(busyTimesPerson1).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(0)));
+    doReturn(busyTimesPerson2).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(1)));
+
+    List<TimePeriod> expectedReturn = Arrays.asList(
+      timePeriodFromDatestring(TIME_10AM, TIME_12PM),
+      timePeriodFromDatestring(TIME_4PM, TIME_6PM),
+      timePeriodFromDatestring(TIME_8PM, TIME_10AM_NEXTDAY)
+    );
+
+    List<TimePeriod> result = proposerSpy.proposeTimes();
+
+    // Assert list sizes are the same
+    assertEquals(expectedReturn.size(), result.size());
+    // Assert that both expected return and actual result are the same
+    assertTrue(result.equals(expectedReturn));
+  }
+
+  // Only one time slot possible considering all the busy periods
+  @Test
+  public void justEnoughRoom() throws Exception {
+    List<TimePeriod> busyTimesPerson1 = Arrays.asList(
+      timePeriodFromDatestring(TIME_10AM, TIME_12PM),
+      timePeriodFromDatestring(TIME_2PM, TIME_8PM),
+      timePeriodFromDatestring(TIME_10PM, TIME_10AM_NEXTDAY)
+    );
+    List<TimePeriod> busyTimesPerson2 = Arrays.asList(
+      timePeriodFromDatestring(TIME_1230PM, TIME_2PM),
+      timePeriodFromDatestring(TIME_1230PM, TIME_2PM),
+      timePeriodFromDatestring(TIME_8PM, TIME_10PM)
+    );
+
+    doReturn(busyTimesPerson1).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(0)));
+    doReturn(busyTimesPerson2).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(1)));
+
+    List<TimePeriod> expectedReturn = Arrays.asList(
+      timePeriodFromDatestring(TIME_12PM, TIME_1230PM)
+    );
+
+    List<TimePeriod> result = proposerSpy.proposeTimes();
+
+    // Assert list sizes are the same
+    assertEquals(expectedReturn.size(), result.size());
+    // Assert that both expected return and actual result are the same
+    assertTrue(result.equals(expectedReturn));
+  }
+
+  // No busy periods detected for all guests across the time period
+  @Test
+  public void noBusyPeriods() throws Exception {
+    List<TimePeriod> busyTimes = Arrays.asList();
+
+    doReturn(busyTimes).when(proposerSpy)
+        .freebusyRequest(anyString());
+
+    List<TimePeriod> expectedReturn = Arrays.asList(
+      timePeriodFromDatestring(TIME_10AM, TIME_10AM_NEXTDAY)
+    );
+
+    List<TimePeriod> result = proposerSpy.proposeTimes();
+
+    // Assert list sizes are the same
+    assertEquals(expectedReturn.size(), result.size());
+    // Assert that both expected return and actual result are the same
+    assertTrue(result.equals(expectedReturn));
+  }
+
+  // No timeslots long enough for the meeting to occur
+  @Test
+  public void notEnoughRoom() throws Exception {
+    List<TimePeriod> busyTimesPerson1 = Arrays.asList(
+      timePeriodFromDatestring(TIME_10AM, TIME_12PM),
+      timePeriodFromDatestring(TIME_2PM, TIME_8PM),
+      timePeriodFromDatestring(TIME_10PM, TIME_10AM_NEXTDAY)
+    );
+    List<TimePeriod> busyTimesPerson2 = Arrays.asList(
+      timePeriodFromDatestring(TIME_12PM, TIME_2PM),
+      timePeriodFromDatestring(TIME_1230PM, TIME_2PM),
+      timePeriodFromDatestring(TIME_8PM, TIME_10PM)
+    );
+
+    doReturn(busyTimesPerson1).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(0)));
+    doReturn(busyTimesPerson2).when(proposerSpy)
+        .freebusyRequest(eq(CAL_ID.get(1)));
+
+    List<TimePeriod> expectedReturn = Arrays.asList();
+    List<TimePeriod> result = proposerSpy.proposeTimes();
+
+    // Assert list sizes are the same
+    assertEquals(expectedReturn.size(), result.size());
+    // Assert that both expected return and actual result are the same
+    assertTrue(result.equals(expectedReturn));
+  }
+
+  // Throws GoogleJsonResponseException on API errors (e.g. invalid key).
+  @Test(expected = GoogleJsonResponseException.class)
+  public void throwsErrorOnAPIFailure() throws Exception {
+    // Do NOT stub the freebusyRequest function - call through the 
+    // proposeTimes() function on the real 'proposer'. 
+    // There is no API key provided in these tests, so API calls 
+    // should return an error.
+    List<TimePeriod> result = proposer.proposeTimes();
+  }
+
+  // Tests for freebusyRequest()
+  @Test(expected = GoogleJsonResponseException.class)
+  public void verifyCorrectAPICalls() throws Exception {
+    // This will throw an exception, since fake API key provided
+    proposer.freebusyRequest("test@example.com");
+
+    // Verify that the correct API call was made
+    FreeBusyRequestItem currentCalItem = 
+        new FreeBusyRequestItem().setId("test@example.com");
+    FreeBusyRequest req = new FreeBusyRequest()
+        .setTimeMin(new DateTime(startTime))
+        .setTimeMax(new DateTime(endTime))
+        .setItems(Arrays.asList(currentCalItem));
+    
+    verify(serviceSpy).freebusy().query(req).setKey(FAKE_API_KEY).execute();
+  }
+  
+  private TimePeriod timePeriodFromDatestring(String start, String end) 
+      throws ParseException {
+    SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+    DateTime startDateTime = 
+        new DateTime(FORMATTER.parse(start), TimeZone.getTimeZone(TIMEZONE));
+    DateTime endDateTime = 
+        new DateTime(FORMATTER.parse(end), TimeZone.getTimeZone(TIMEZONE));
+    TimePeriod time = new TimePeriod()
+        .setStart(startDateTime)
+        .setEnd(endDateTime);
+    return time;
+  }
+}
